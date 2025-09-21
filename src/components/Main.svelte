f6b6af8d76275ad3aafb2fd5d46ef0008c96db26
Konrad <malarakonrad05@gmail.com>
2025-09-21 23:20:55 +0100

    just little things

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -59,6 +59,7 @@ import Step from "./Step.svelte";
 
 
     <!-- Projects Section -->
+    <!-- ADD A CAROUSEL MAYBE IDK-->
     <section id="projects" class="py-20 lg:py-32 flex flex-col gap-24  ">
         <div class="flex flex-col gap-2 text-center">
             <h6 class="text-lg sm:text-xl md:text-2xl">
